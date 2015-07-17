e46ba74da7b35ae68e9953c6e4d31aa26c0c4f97
Nuklearwurst <agbrjonas@web.de>
2015-07-17 13:05:54 +0200

    started working on Items

--- src/main/java/com/fravokados/peacehammer/item/ItemDwarfMetal.java
+++ src/main/java/com/fravokados/peacehammer/item/ItemDwarfMetal.java
@@ -1,7 +1,35 @@
 package com.fravokados.peacehammer.item;
 
+import net.minecraft.client.renderer.texture.IIconRegister;
+import net.minecraft.creativetab.CreativeTabs;
+import net.minecraft.item.Item;
+import net.minecraft.item.ItemStack;
+import net.minecraft.util.IIcon;
+
+import java.util.List;
+
 /**
  * @author Nuklearwurst
  */
-public class ItemDwarfMetal extends ItemPH {
+public class ItemDwarfMetal extends ItemPHMultiType {
+
+	@Override
+	public IIcon getIconFromDamage(int p_77617_1_) {
+		return super.getIconFromDamage(p_77617_1_);
+	}
+
+	@Override
+	public void getSubItems(Item item, CreativeTabs tabs, List list) {
+
+	}
+
+	@Override
+	protected String getUnlocalizedNameForItem(ItemStack stack) {
+		return null;
+	}
+
+	@Override
+	public void registerIcons(IIconRegister reg) {
+
+	}
 }
---
+++ src/main/java/com/fravokados/peacehammer/item/ItemIngredient.java
@@ -0,0 +1,7 @@
+package com.fravokados.peacehammer.item;
+
+/**
+ * @author Nuklearwurst
+ */
+public class ItemIngredient extends ItemPH {
+}
